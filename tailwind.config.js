6ef65099a5c7599ef6c1eabc176eb89a20a5e550
NaNa2005 <hovvathana1@gmail.com>
2023-04-23 21:37:58 +0700

    UI demo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontFamily: {
+      primary: "Poppins",
+    },
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        gradientBg: "url('/bg.png')",
+      },
+      keyframes: {
+        shake: {
+          "0%": {
+            transform: "translate(3px, 0)",
+          },
+          "50%": {
+            transform: "translate(-3px, 0)",
+          },
+          "100%": {
+            transform: "translate(0, 0)",
+          },
+        },
+      },
+      animation: {
+        shake: "shake 150ms 2 linear",
       },
     },
   },
   plugins: [],
-}
+};
