e9ed7e6cdcd37e4d854bff409a70bb05a12baeb5
NaNa2005 <hovvathana1@gmail.com>
2023-04-24 09:21:50 +0700

    Implement searching feature

--- src/app/components/SearchInput.tsx
+++ src/app/components/SearchInput.tsx
@@ -1,8 +1,10 @@
 "use client";
 
 import { useClickOutside } from "@/util";
-import { UilSearch } from "@iconscout/react-unicons";
-import { useRef } from "react";
+import { UilLocationPoint, UilSearch } from "@iconscout/react-unicons";
+import axios from "axios";
+import { useEffect, useRef, useState } from "react";
+import { SearchResultModel } from "../model/SearchResultModel";
 
 type SearchInputProps = {
   setIsSearchOpen: () => void;
@@ -10,22 +12,61 @@ type SearchInputProps = {
 
 function SearchInput({ setIsSearchOpen }: SearchInputProps) {
   const inputRef = useRef(null);
+  const [searchInput, setSearchInput] = useState("");
+  const [searchResult, setSearchResult] = useState<SearchResultModel[]>([]);
 
   useClickOutside(inputRef, () => {
     setIsSearchOpen(false);
   });
 
+  useEffect(() => {
+    const getSearchData = async () => {
+      const { data } = await axios.get(
+        `https://geocoding-api.open-meteo.com/v1/search?name=${searchInput}`
+      );
+      setSearchResult(data.results);
+      return data;
+    };
+    getSearchData();
+  }, [searchInput]);
+
+  console.log(searchResult);
+
   return (
-    <div className="flex w-full cursor-pointer border p-2 rounded-lg gap-1 border-white ">
-      <UilSearch size="30" color="#fff" />
-      <input
-        ref={inputRef}
-        className="w-full bg-transparent outline-none font-medium placeholder-slate-200"
-        type="text"
-        placeholder="Search by location or coordinates"
-        onChange={(e) => {}}
-      />
-    </div>
+    <>
+      <div className="w-full relative" ref={inputRef}>
+        <div className="flex w-full cursor-pointer border p-2 rounded-lg gap-1 border-white ">
+          <UilSearch size="30" color="#fff" />
+          <input
+            className="w-full bg-transparent outline-none font-medium placeholder-slate-200"
+            type="text"
+            placeholder="Search by location or coordinates"
+            onChange={(e) => setSearchInput(e.target.value)}
+          />
+        </div>
+        {searchResult?.length > 0 && (
+          <div className="absolute left-0 top-[60px] text-black bg-white bg-opacity-40 backdrop-filter backdrop-blur-lg z-[99] w-full p-4 rounded-lg shadow-lg max-h-[500px] overflow-y-auto">
+            {searchResult?.map((search, i) => (
+              <div
+                key={search?.id}
+                className="flex w-[100%] items-center gap-2 p-3 border-b last:border-none  border-gray-400 cursor-pointer hover:bg-blue-100 hover:rounded-lg hover:border-blue-100 "
+                onClick={() => {}}
+              >
+                <UilLocationPoint size="30" color="#000" />
+
+                <div>
+                  <h3 className="md:text-md text-sm">{search?.name}</h3>
+                  <p className="md:text-md text-sm">
+                    {search.admin1} {search.country_code} ({search.country}),
+                    elevation {search.elevation} m
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </>
   );
 }
 
---
+++ src/app/model/SearchResultModel.ts
@@ -0,0 +1,14 @@
+export interface SearchResultModel {
+  id?: number;
+  name?: string;
+  latitude?: number;
+  longitude?: number;
+  elevation?: number;
+  feature_code?: string;
+  country_code?: string;
+  timezone?: string;
+  population?: number;
+  country_id?: number;
+  country?: string;
+  admin1?: string;
+}
